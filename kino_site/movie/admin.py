f1c568dcc3fb939aed9991a90b44fc9940ff318c
unknown <rmyagdeev@mail.ru>
2021-11-19 13:04:59 +0300

    models

--- kino_site/movie/admin.py
+++ kino_site/movie/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Reviews, Rating, RatingStar, MovieShots, Movie, Genre, Actor, Category
 # Register your models here.
+models = [Reviews, Rating, RatingStar, MovieShots, Movie, Genre, Actor, Category]
+admin.site.register(models)
