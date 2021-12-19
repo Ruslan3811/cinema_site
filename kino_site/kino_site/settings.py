4863b3dd4bdf1d4c7b368650081011ff8c45211d
unknown <rmyagdeev@mail.ru>
2021-12-19 18:54:27 +0300

    added social app

--- kino_site/kino_site/settings.py
+++ kino_site/kino_site/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'modeltranslation',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.vk',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
